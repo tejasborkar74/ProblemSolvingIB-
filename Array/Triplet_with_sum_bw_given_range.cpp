62135626ddf757311e32aac497b85a857a7600c9
unknown <tejasborkar74@gmail.com>
2020-05-31 17:59:57 +0530

    Triplets with Sum between given range

--- Array/Triplet_with_sum_bw_given_range.cpp
+++ Array/Triplet_with_sum_bw_given_range.cpp
@@ -1,15 +1,15 @@
 int Solution::solve(vector<string> &A)
 {
-    vector<float> a,b,c;
+    vector<double> a,b,c;
     for(int i=0;i<A.size();i++)
     {
-        float val=std::stof(A[i]);
+        double val=std::stod(A[i]);
 
-        if(val>0 && val<(2.0/3))
+        if(val>0.0 && val<(2.0/3))
         {
             a.push_back(val);
         }
-        else if(val>=(2.0/3) && val< 1)
+        else if(val>=(2.0/3) && val< 1.0)
         {
             b.push_back(val);
         }
@@ -27,14 +27,14 @@ int Solution::solve(vector<string> &A)
 
     if(a.size()>=3)
     {
-        float temp=a[n-1] + a[n-2] + a[n-3];
+        double temp=a[n-1] + a[n-2] + a[n-3];
 
         if(temp>1 && temp<2)return 1;
     }
 
     if(a.size()>=2 && b.size()>=1)
     {
-        float temp=a[n-1] + a[n-2] + b[m-1];
+        double temp=a[n-1] + a[n-2] + b[0];
 
         if(temp>1 && temp<2)return 1;
 
@@ -42,19 +42,19 @@ int Solution::solve(vector<string> &A)
     if(a.size()>=2 && c.size()>=1)
     {
 
-        float temp=a[n-1] + a[n-2] + c[k-1];
+        double temp=a[1] + a[0] + c[0];
 
         if(temp>1 && temp<2)return 1;
     }
     if(b.size()>=2 && a.size()>=1)
     {
-        float temp=b[m-1] + b[m-2] + a[n-1];
+        double temp=b[0] + b[1] + a[n-1];
 
         if(temp>1 && temp<2)return 1;
     }
     if(a.size()>=1 && b.size()>=1 && c.size()>=1)
     {
-          float temp=a[n-1] + b[m-1] + c[k-1];
+          double temp=a[0] + b[0] + c[0];
 
             if(temp>1 && temp<2)return 1;
     }
