d9f38863aa6505c4295f949fd461b9ea7c497530
unknown <tejasborkar74@gmail.com>
2020-06-07 12:54:16 +0530

    Iterative solution added

--- Linked Lists/Merge_Two_Sorted_List.cpp
+++ Linked Lists/Merge_Two_Sorted_List.cpp
@@ -32,3 +32,64 @@ ListNode* Solution::mergeTwoLists(ListNode* a, ListNode* b)
     return merge(a,b);
 
 }
+
+
+///ITERATIVE SOLUTION/////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ListNode* Solution::mergeTwoLists(ListNode* a, ListNode* b)
+{
+    ListNode *c,*ans;
+    if(a==NULL)return b;
+
+     if(b==NULL)return a;
+
+     if(a->val<b->val)
+     {
+         c=a;
+         ans=c;
+         a=a->next;
+     }
+     else
+     {
+         c=b;
+         ans=c;
+         b=b->next;
+     }
+
+     while(a&&b)
+     {
+         if(a->val < b->val)
+         {
+             c->next=a;
+             a=a->next;
+             c=c->next;
+         }
+         else
+         {
+            c->next=b;
+            b=b->next;
+            c=c->next;
+         }
+     }
+
+     if(a)
+     {
+         c->next=a;
+     }
+     else if(b)
+     {
+         c->next=b;
+     }
+
+     return ans;
+
+}
